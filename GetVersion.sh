d7c610a57e85c8c92f54a582e844751524a8b13f
David Wang <davidwang@kkstream.com>
2019-10-07 11:47:44 +0800

    Remove redundant sed options.

--- GetVersion.sh
+++ GetVersion.sh
@@ -2,9 +2,9 @@
 function GetVersion
 {
 	PROJECT_PBXPROJ_PATH=$1
-	INFOPLIST_FILE_PATH=$(echo $2 | sed -e 's/\//\\\//g')
+	INFOPLIST_FILE_PATH=$(echo $2 | sed 's/\//\\\//g')
 	version=$(sed -n -e "/INFOPLIST_FILE = \"${INFOPLIST_FILE_PATH}\"/,/MARKETING_VERSION/p" $PROJECT_PBXPROJ_PATH\
-			| sed -n -e '/MARKETING_VERSION.*/p'\
+			| sed -n '/MARKETING_VERSION.*/p'\
 			| sed 's/.*MARKETING_VERSION = //g'\
 			| sed 's/;//g'\
 			| head -1)
